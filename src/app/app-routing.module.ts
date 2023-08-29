029e34ba3b79308d77bbda8a083fe38ac6c3fa35
kennymartinez19k <kennymartinez19k@gmail.com>
2023-08-29 14:12:49 -0400

    add part of home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 import { ExitosoComponent } from './components/exitoso/exitoso.component';
+
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 const routes: Routes = [
   { path: '', component: RegisterComponent },
   {path:'register', component: RegisterComponent},
-  {path:'exito', component: ExitosoComponent}
-
+  {path:'exito', component: ExitosoComponent},
+  { path: 'home', component: HomeComponent },
+  {path: '',component: LoginComponent}
+  
 ];
 
 @NgModule({
